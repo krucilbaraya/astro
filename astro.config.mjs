ac7024943bb8a21e3fd5c64834b5d2e1204e78d3
krucilbaraya <krucilbaraya@gmail.com>
2025-02-07 14:56:47 +0700

    Added foo to the bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 import node from "@astrojs/node";
@@ -6,7 +7,9 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  integrations: [image()],
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  experimental: { assets: true },
 });
